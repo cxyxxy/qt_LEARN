4885550a4fc0992fd1a7570c1b589969a400442c
cxyxxy <2217023139@qq.com>
2024-10-01 17:58:42 +0800

    【进展】:绘制出基本图形，能够活动

--- Desktop_pets/desktop_pets.cpp
+++ Desktop_pets/desktop_pets.cpp
@@ -5,4 +5,283 @@ Desktop_pets::Desktop_pets(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+
+    setStatusBar(nullptr);
+    setMenuBar(nullptr);
+    removeToolBar(ui.mainToolBar);
+
+    setWindowFlags(Qt::FramelessWindowHint | Qt::Tool);//去掉窗口标题
+    setAttribute(Qt::WA_TranslucentBackground);//设置背景透明
+    Qt::WindowFlags m_flags = windowFlags();//保持窗口置顶1
+    setWindowFlags(m_flags | Qt::WindowStaysOnTopHint);//保持窗口置顶2
+
+    int coordX, coordY;//桌面坐标
+    QFile file("./file/file.dat");
+    file.open(QIODevice::ReadOnly);
+    QDataStream in(&file);
+    if (file.isOpen())//读取体型、装扮编号参数、相对桌面坐标
+        in >> m_size >> m_bodyNum >> m_earsNum >> coordX >> coordY;
+    else {
+        m_size = 400;
+        m_bodyNum = 0;
+        m_earsNum = 0;
+        coordX = x();
+        coordY = y();
+    }
+    file.close();
+    move(coordX, coordY);
+
+    m_timer = new QTimer;
+    m_timer->start(40);//动画速度
+    connect(m_timer, &QTimer::timeout, this, &Desktop_pets::eyesMovement);//关联眼部动作
+
+    imageLoad();//载入部位图片
+    eyesMovementLoad();//载入表情图片
+    specialMovementLoad();//载入特殊动作图片
+
+    m_bodyImage = new QLabel(this);//身体图片指针
+    m_eyesImage = new QLabel(this);//眼部图片指针
+    m_stripeImage = new QLabel(this);//屏幕遮盖条纹图片指针
+    m_earsImage = new QLabel(this);//耳朵图片指针
+
+    imageSet(m_bodyImage, m_body[m_bodyNum]);
+
+    imageSet(m_eyesImage, m_eyes);
+
+    if (m_size > 140) 
+    {
+        imageSet(m_stripeImage, m_stripe);
+        m_stripeImage->show();
+    }
+    else m_stripeImage->hide();
+
+    imageSet(m_earsImage, m_earsLeft[m_earsNum]);
 }
+
+void Desktop_pets::mousePressEvent(QMouseEvent* event)
+{
+    if (event->button() == Qt::LeftButton)
+    {
+        m_bPressed = true;
+        m_mouseStartPoint = event->globalPos();
+        m_windowTopLeftPoint = this->frameGeometry().topLeft();
+    }
+}
+
+void Desktop_pets::mouseMoveEvent(QMouseEvent* event)
+{
+    if (this->isMaximized()) //如果当前是最大化，则不允许移动
+        return;
+
+    if (m_bPressed)
+    {
+        QPoint distance = event->globalPos() - m_mouseStartPoint;
+        this->move(m_windowTopLeftPoint + distance);
+    }
+}
+
+void Desktop_pets::mouseReleaseEvent(QMouseEvent* event)
+{
+
+    if (event->button() == Qt::LeftButton)
+    {
+        m_bPressed = false;
+    }
+}
+
+void Desktop_pets::imageLoad()
+{
+    //载入装扮图片
+    m_body.push_back(QPixmap(QString(":/images/appearance/body/def_body.png")));
+    m_body.push_back(QPixmap(QString(":/images/appearance/body/blue_body.png")));
+    m_body.push_back(QPixmap(QString(":/images/appearance/body/pink_body.png")));
+    m_body.push_back(QPixmap(QString(":/images/appearance/body/icefire_body.png")));
+    m_body.push_back(QPixmap(QString(":/images/appearance/body/cat_body.png")));
+    m_body.push_back(QPixmap(QString(":/images/appearance/body/Gundam_body.png")));
+    m_body.push_back(QPixmap(QString(":/images/appearance/body/drill_body.png")));
+    m_body.push_back(QPixmap(QString(":/images/appearance/body/angel_body.png")));
+
+    m_earsLeft.push_back(QPixmap(QString(":/images/appearance/ears/def_ears1.png")));
+    m_earsLeft.push_back(QPixmap(QString(":/images/appearance/ears/blue_ears1.png")));
+    m_earsLeft.push_back(QPixmap(QString(":/images/appearance/ears/pink_ears1.png")));
+    m_earsLeft.push_back(QPixmap(QString(":/images/appearance/ears/icefire_ears1.png")));
+    m_earsLeft.push_back(QPixmap(QString(":/images/appearance/ears/cat_ears1.png")));
+    m_earsLeft.push_back(QPixmap(QString(":/images/appearance/ears/Gundam_ears1.png")));
+    m_earsLeft.push_back(QPixmap(QString(":/images/appearance/ears/drill_ears1.png")));
+    m_earsLeft.push_back(QPixmap(QString(":/images/appearance/ears/angel_ears1.png")));
+
+    m_earsRight.push_back(QPixmap(QString(":/images/appearance/ears/def_ears2.png")));
+    m_earsRight.push_back(QPixmap(QString(":/images/appearance/ears/blue_ears2.png")));
+    m_earsRight.push_back(QPixmap(QString(":/images/appearance/ears/pink_ears2.png")));
+    m_earsRight.push_back(QPixmap(QString(":/images/appearance/ears/icefire_ears2.png")));
+    m_earsRight.push_back(QPixmap(QString(":/images/appearance/ears/cat_ears2.png")));
+    m_earsRight.push_back(QPixmap(QString(":/images/appearance/ears/Gundam_ears2.png")));
+    m_earsRight.push_back(QPixmap(QString(":/images/appearance/ears/drill_ears2.png")));
+    m_earsRight.push_back(QPixmap(QString(":/images/appearance/ears/angel_ears2.png")));
+
+    m_eyes.load(":/images/appearance/eyes/def_eyes.png");
+    m_stripe.load(":/images/appearance/stripe.png");
+}
+
+void Desktop_pets::eyesMovementLoad()
+{
+    m_faceNum.push_back(9);//帧数-例：9代表9帧
+    m_faceNum.push_back(0);//起始位置-例：0代表该表情第一张图片下标
+    for (int i = 1; i <= m_faceNum[0]; i++)//表情1-眨眼
+        m_movement.push_back(QPixmap(QString(":/images/movement/blink/%1.png").arg(i)));
+
+    m_faceNum.push_back(12);
+    m_faceNum.push_back(9);
+    for (int i = 1; i <= m_faceNum[2]; i++)//表情2-心动
+        m_movement.push_back(QPixmap(QString(":/images/movement/heart/%1.png").arg(i)));
+
+    m_faceNum.push_back(16);
+    m_faceNum.push_back(21);
+    for (int i = 1; i <= m_faceNum[4]; i++)//表情3-疑惑
+        m_movement.push_back(QPixmap(QString(":/images/movement/question/%1.png").arg(i)));
+
+    m_faceNum.push_back(15);
+    m_faceNum.push_back(37);
+    for (int i = 1; i <= m_faceNum[6]; i++)//表情4-闭眼
+        m_movement.push_back(QPixmap(QString(":/images/movement/closeEyes/%1.png").arg(i)));
+
+    m_faceNum.push_back(9);
+    m_faceNum.push_back(52);
+    for (int i = 1; i <= m_faceNum[8]; i++)//表情5-单眨眼
+        m_movement.push_back(QPixmap(QString(":/images/movement/wink/%1.png").arg(i)));
+
+    m_face = -1;//表情序号初始化为-1，不生效
+    m_faceSum = 5;//表情数量
+}
+
+void Desktop_pets::specialMovementLoad()
+{
+    for (int i = 1; i <= 11; i++)
+        m_specialMovement.push_back(QPixmap(QString(":/images/movement/error/%1.png").arg(i)));
+    for (int i = 1; i <= 22; i++)
+        m_specialMovement.push_back(QPixmap(QString(":/images/movement/fly/%1.png").arg(i)));
+
+    m_specialMove = -1;
+}
+
+void Desktop_pets::imageSet(QLabel* image, QPixmap map)
+{
+    //根据size设定各图片大小和坐标
+    image->setPixmap(map.scaled(m_size, m_size));//使用scaled修改图片大小，能避免图片因缩放模糊
+    image->setScaledContents(true);
+    image->resize(m_size, m_size);
+    image->move(this->frameGeometry().width() / 2 - m_size / 2,
+        this->frameGeometry().height() / 2 - m_size / 2);
+}
+
+void Desktop_pets::eyesMovement()
+{
+    //各种静态变量，用于计数、记录状态等↓
+    static int flag = 0, second1 = 0, second2 = 0, earSwitch = 1;
+    int valve = qrand() % 200;
+    if (m_face < 0 && m_specialMove < 0) {//控制眨眼动作
+        second1++;
+        if (second1 >= valve && valve > 100) {
+            m_face = 0;
+            second1 = 0;
+        }
+    }
+
+    second2++;//控制耳朵的动画
+    if (second2 > 40 && earSwitch) {
+        m_earsImage->setPixmap(m_earsRight[m_earsNum].scaled(m_size, m_size));
+        earSwitch = 0;
+        second2 = 0;
+    }
+    else if (second2 > 10 && !earSwitch) {
+        m_earsImage->setPixmap(m_earsLeft[m_earsNum].scaled(m_size, m_size));
+        earSwitch = 1;
+        second2 = 0;
+    }
+
+    if (m_face != -1) {//控制表情变化
+        int num = m_faceNum[m_face * 2], start = m_faceNum[m_face * 2 + 1];
+        flag++;
+        if (flag < num)
+            m_eyesImage->setPixmap(
+                m_movement[start + flag].scaled(m_size, m_size));
+        else
+            m_eyesImage->setPixmap(
+                m_movement[start - flag + (num - 1) * 2].scaled(m_size, m_size));
+
+        if (flag >= (num - 1) * 2) {
+            flag = 0;
+            m_face = -1;
+            m_eyesImage->setPixmap(m_eyes.scaled(m_size, m_size));
+        }
+    }
+    //if (!dressWindow->isHidden()) {
+    //    //从换装窗口中获取bodyNum、earsNum参数
+    //    if (bodyNum != dressWindow->getBodyNum()) {
+    //        bodyNum = dressWindow->getBodyNum();
+    //        bodyImage->setPixmap(body[bodyNum].scaled(m_size, m_size));
+    //        saveData();
+    //    }
+    //    if (earsNum != dressWindow->getEarsNum()) {
+    //        earsNum = dressWindow->getEarsNum();
+    //        earsImage->setPixmap(ears1[earsNum].scaled(m_size, m_size));
+    //        saveData();
+    //    }
+    //}
+
+    //if (!setWindow->isHidden()) {//从设置窗口中获取size参数
+    //    if (size != setWindow->getSize()) {
+    //        size = setWindow->getSize();
+
+    //        imageSet(bodyImage, body[bodyNum]);
+    //        imageSet(eyesImage, eyes);
+    //        if (size > 140) {
+    //            imageSet(stripeImage, stripe);
+    //            stripeImage->show();
+    //        }
+    //        else
+    //            stripeImage->hide();
+    //        imageSet(earsImage, ears1[earsNum]);
+
+    //        saveData();
+    //        reInitBtn();
+    //    }
+    //}
+    if (m_specialMove > -1)//特殊动作
+        specialMovement();
+}
+
+void Desktop_pets::specialMovement() {
+    static int flag = 0;
+
+    if (m_specialMove == 0) {//动作-error
+        if (flag % 20 == 0 && flag <= 200)
+            m_eyesImage->setPixmap(m_specialMovement[flag / 20].scaled(m_size, m_size));
+        else if (flag > 300) {
+            m_eyesImage->setPixmap(m_eyes.scaled(m_size, m_size));
+            flag = 0;
+            m_specialMove = -1;
+            return;
+        }
+    }
+    flag++;
+}
+
+void Desktop_pets::saveData()
+{
+    QFile file("./file/file.dat");
+    file.open(QIODevice::WriteOnly);
+    QDataStream out(&file);
+    out << m_size << m_bodyNum << m_earsNum << x() << y();//存储体型、装扮编号参数、窗口坐标
+    file.close();
+}
+
+
+
+
+
+
+
+
+
+
--- Desktop_pets/desktop_pets.h
+++ Desktop_pets/desktop_pets.h
@@ -1,6 +1,27 @@
-#pragma once
+#ifndef __DESKTOP_PET_H__
+#define __DESKTOP_PET_H__
 
 #include <QtWidgets/QMainWindow>
+#include <QStyleOption>
+#include <QPoint>
+#include <QBitmap>
+#include <QPainter>
+#include <QBrush>
+#include <QFont>
+#include <QPen>
+#include<QMouseEvent>
+#include<QtGlobal>
+#include <QLabel>
+#include<QPoint>
+#include<QTimer>
+#include<QPushButton>
+#include<QCalendarWidget>
+#include<QFile>
+#include<QDataStream>
+#include<QSystemTrayIcon>
+#include<QPixmap>
+
+
 #include "ui_desktop_pets.h"
 
 class Desktop_pets : public QMainWindow
@@ -10,6 +31,50 @@ class Desktop_pets : public QMainWindow
 public:
     Desktop_pets(QWidget *parent = Q_NULLPTR);
 
+
+protected:
+    void mousePressEvent(QMouseEvent* event) override;
+    void mouseMoveEvent(QMouseEvent* event) override;
+    void mouseReleaseEvent(QMouseEvent* event) override;
+    void imageLoad();//各部位图片载入
+    void eyesMovementLoad();//眼部动作载入
+    void specialMovementLoad();//特殊动作事件
+    inline void imageSet(QLabel* image, QPixmap map);//设置各部位图片
+    void eyesMovement();//眼部动作表情
+    void specialMovement();//特殊动作事件
+    void saveData();//存储数据
+
+
+
 private:
     Ui::Desktop_petsClass ui;
+
+    //实现窗口拖拽
+    bool m_bPressed;
+    QPoint m_mouseStartPoint;
+    QPoint m_windowTopLeftPoint;
+
+    int m_size;//体型大小
+    int m_bodyNum, m_earsNum;//身体和耳朵对应装扮序号
+    QTimer* m_timer;//定时器:定时触发动作
+    std::vector<QPixmap> m_body;//身体部位对应图片容器
+    std::vector<QPixmap> m_earsLeft;//左耳朵对应图片容器
+    std::vector<QPixmap> m_earsRight;//右耳朵对应图片容器
+    QPixmap m_eyes, m_stripe;//眼睛和眼部遮罩
+    int m_face;//表情序号
+    int m_faceSum;//表情数量
+    std::vector<int> m_faceNum;//每个表情对应帧数与起始位置
+    std::vector<QPixmap> m_movement;//表情图片容器
+    int m_specialMove;//特殊动作序号
+    std::vector<QPixmap> m_specialMovement;//特殊动作图片容器
+    //各部件对应标签
+    QLabel* m_bodyImage;
+    QLabel* m_earsImage;
+    QLabel* m_eyesImage;
+    QLabel* m_stripeImage;
+
+
 };
+
+
+#endif // __DESKTOP_PET_H__
